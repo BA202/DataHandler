166fefa34eb4732f0bcab7df7e9daf79faea218d
TobiasRothlin <tobias@rothlin.com>
2022-03-03 10:31:32 +0100

    Added setup file for pip

--- setup.py
+++ setup.py
@@ -9,4 +9,16 @@ setup(name='data_handler',
       author='Tobias Rothlin',
       author_email='tobias@rothlin.com',
       url='https://github.com/BA202/DataHandler',
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python',
+            'Intended Audience :: System Administrators',
+            'Topic :: Terminals',
+            ],
+      download_url="http://github.com/downloads/bitly/data_hacks/data_hacks-%s.tar.gz" % version,
+      scripts = ['data_hacks/histogram.py', 
+                'data_hacks/ninety_five_percent.py',
+                'data_hacks/run_for.py',
+                'data_hacks/bar_chart.py',
+                'data_hacks/sample.py']
      )
