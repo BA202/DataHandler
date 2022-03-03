8b610672004edece03631fb29ec48516a10914c7
TobiasRothlin <tobias@rothlin.com>
2022-03-03 10:29:11 +0100

    Added setup file for pip

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+version='0.0.3'
+
 setuptools.setup(
     name='data_handler',
     version='0.0.3',
