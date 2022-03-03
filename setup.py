6b6febaaa798655866be8988ed40f794a50e6cc6
TobiasRothlin <tobias@rothlin.com>
2022-03-03 09:55:52 +0100

    Added setup file for pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DataHandler',
+    version='1.0',
+
+    url='https://github.com/BA202/DataHandler',
+    author='Tobias Rothlin',
+    author_email='tobias@rothlin.com',
+
+    py_modules=['DataHandler'],
+)
